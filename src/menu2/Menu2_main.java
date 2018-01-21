c88ce196f9ef6f3ac78b6f76f1f9de9b65bef92b
ts719soccer@gmail.com <ts719soccer@gmail.com>
2018-01-22 06:09:06 +0900

    Menu2

---
+++ src/menu2/Menu2_main.java
@@ -0,0 +1,27 @@
+package menu2;
+
+import menu2.Menu2_view;
+
+public class Menu2_main {
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		Menu2_view menu = new Menu2_view();
+
+        menu.setBounds(5,5,755,455);
+
+        menu.setVisible(true);
+
+
+
+
+
+	}
+
+
+
+}
